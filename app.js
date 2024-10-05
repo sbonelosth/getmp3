0a17b65217e69a046aedeb5e0a1d69fdab4d4a62
sbonelosth <sbonelosthole97@gmail.com>
2024-10-05 14:06:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,40 @@
-// required packages
-
+// required dependencies
 const express = require("express")
 const axios = require('axios')
 require("dotenv").config()
 
 // creating the express server
-
 const app = express()
 
 // server port number
-
 const PORT = process.env.PORT || 3000;
 
-// setting template engine
-
+// setting the view engine to ejs which is used to render the html
 app.set("view engine", "ejs")
 app.use(express.static("views"))
 
 // required for parsing html data for POST request
-
 app.use(express.urlencoded({
   extended: true
 }))
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
+// routes: a route for the home page
+app.get("/", (_, res) => {
   res.render("index")
 })
 
 app.post("/download", async (req, res) => {
-  const videoIdx = req.body.videoID;
-  const videoIdMatch = videoIdx.match(/^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(?:-nocookie)?\.com|youtu.be))(/(?:[\w-]+?v=|embed\/|live\/|v\/)?)([\w-]+)(\S+)?$/);
-  
-  const videoId = videoIdMatch ? videoIdMatch[5] : null;
+  const videoIdx = req.body.videoID
+  const videoIdMatch = videoIdx.match(/(?:youtu\.be\/|youtube\.com\/(?:.*v=|.*\/v\/|.*\/embed\/|.*\/shorts\/|.*\/watch\?v=))([a-zA-Z0-9_-]{11})/)
+  const videoId = videoIdMatch ? videoIdMatch[1] : null
   if (
     videoId === undefined ||
     videoId === "" ||
     videoId === null
   ) {
-    console.log('Invalid url')
+    console.log('Invalid URL')
     return res.render("index", { success: false, message: "Enter a valid YouTube URL" })
   } else {
     const fetchAPI = await axios.request(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`, {
@@ -47,7 +43,7 @@ app.post("/download", async (req, res) => {
         "x-rapidapi-key": process.env.API_KEY,
         "x-rapidapi-host": process.env.API_HOST
       }
-    });
+    })
     const fetchResponse = await fetchAPI.data;
 
     if (fetchResponse.msg === "success") {
@@ -62,7 +58,6 @@ app.post("/download", async (req, res) => {
 })
 
 // starting the server
-
 app.listen(PORT, () => {
-  console.log(`Server strated on port ${PORT}...`)
+  console.log(`Server started on http://localhost:${PORT}...`)
 })
